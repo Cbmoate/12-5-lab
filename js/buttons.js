143bba599c14c41f1ea90e65d3b65bc515a16659
Alexg2195 <alexg2195@gmail.com>
2015-12-05 13:47:02 -0500

    testing google maps api

--- js/buttons.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-  $(".btn-default").on("click", function(e){
-    e.preventDefault();
-    var userAddress = $("#userAddress").val();
-    var googleApiUrl = "https://maps.googleapis.com/maps/api/geocode/json?";
-    googleApiUrl += "key=AIzaSyADnEkRq-1ke4NWknv2pxRajx4f1vGlscg";
-    googleApiUrl += "&address=" + userAddress;
-  });
-});
--- js/space.js
+++ js/space.js
@@ -1,3 +1,20 @@
 $(document).ready(function(){
+  $(".btn-default").on("click", function(e){
+    e.preventDefault();
+    var userAddress = $("#userAddress").val();
+    var googleApiUrl = "https://maps.googleapis.com/maps/api/geocode/json?";
+    googleApiUrl += "key=AIzaSyADnEkRq-1ke4NWknv2pxRajx4f1vGlscg";
+    googleApiUrl += "&address=" + userAddress;
 
+    .ajax({
+      type: "GET",
+      url: "googleApiUrl",
+      success: googleApiSuccess
+    });
+  });
+
+  function googleApiSuccess(responce) {
+    console.log("responce");
+  }
+  });
 });
